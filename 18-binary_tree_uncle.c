dfe744346e1cb722e1e4c75ce7016cebc3acb813
Eben <maradebenezer@gmail.com>
2023-06-01 13:44:58 +0100

    fixed 18

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -1,19 +1,21 @@
 #include "binary_trees.h"
 
 /**
- * 
+ * binary_tree_uncle - find uncle of node
+ * @node: node to find uncle of
+ * Return: uncle, or NULL if node is NULL or no uncle
  */
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
-        binary_tree_t *uncle;
+	binary_tree_t *uncle;
 
-        if (!node || !node->parent || !node->parent->parent
-        || !node->parent->parent->left
-        || !node->parent->parent->right)
-                return (0);
+	if (!node || !node->parent || !node->parent->parent
+	|| !node->parent->parent->left
+	|| !node->parent->parent->right)
+		return (0);
 
-        uncle = node->parent->parent->right;
-        uncle = (node->parent == uncle) ? node->parent->parent->left : uncle;
+	uncle = node->parent->parent->right;
+	uncle = (node->parent == uncle) ? node->parent->parent->left : uncle;
 
-        return uncle;
+	return (uncle);
 }
